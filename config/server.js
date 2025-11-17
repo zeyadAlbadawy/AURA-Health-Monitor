47d594d2c23768e02e186a16a99f55fa1d510234
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-11-17 14:38:17 +0200

    return the server to its original state

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
-const app = require('./app.js');
+const app = require('../app.js');
 
 const DB = process.env.DATABASE_URL.replace(
   '<db_password>',
