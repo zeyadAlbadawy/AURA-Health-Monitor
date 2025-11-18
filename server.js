9362fdebb33e7af116ad92a34fdb1b7960e4a8da
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-11-19 01:16:49 +0200

    enable max duration

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const DB = process.env.DATABASE_URL.replace(
   '<db_password>',
   process.env.DATABASE_PASSWORD
 );
-
+///
 mongoose
   .connect(DB)
   .then(() => console.log(' ✅ DB Connected Successfully!'))
