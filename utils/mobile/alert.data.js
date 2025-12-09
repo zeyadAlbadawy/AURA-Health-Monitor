157af7961c7f48b51452a3ef88caf31d7b7e1424
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-12-09 16:41:09 +0200

    socket connection for sending alert

--- utils/mobile/alert.data.js
+++ utils/mobile/alert.data.js
@@ -4,12 +4,18 @@ const alertEmitter = new AlertEmitter();
 
 // Current alert string
 let currentAlert =
-  'The alert logic which I will push to mobile app will be implemented here!';
+  'The alert logic which I will push to mobile app will be implemented here!!';
 
+let prevAlert = null;
 // update the alert
+
 const updateAlert = (newAlert) => {
-  currentAlert = newAlert;
-  alertEmitter.emit('newAlert', currentAlert);
+  if (newAlert && newAlert !== prevAlert) {
+    prevAlert = newAlert;
+    currentAlert = newAlert;
+    console.log('Emitted new alert:', newAlert);
+    alertEmitter.emit('newAlert', newAlert);
+  }
 };
 
 // get current alert
